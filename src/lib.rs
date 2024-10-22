222cc4672101f264e7998030be24e1e33af66833
Xinling Jing <jingxinling1024@163.com>
2024-10-22 12:40:16 +0800

    3.6-3.7.3.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,29 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::io::Error;
 use std::net::TcpListener;
+use actix_web::web::Form;
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String,
+}
+
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, Error> {
+    println!("The port is: {}", listener.local_addr()?.port());
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
